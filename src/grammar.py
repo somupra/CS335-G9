33b9148c11b7da42cb8f9a5224618e8342de161a
Shashank Agarwal <shashank4120@gmail.com>
2021-03-10 18:52:24 +0530

    Add files via upload

--- src/grammar.py
+++ src/grammar.py
@@ -1,206 +1,169 @@
 import sys
-import ply.yacc as yacc
-from lexrules import tokens
-
-class Node:
-    def __init__(self, type, children=None, leaf=None):
-        self.type = type
-        if children:
-            self.children = children
-        else:
-            self.children = [ ]
-        self.leaf = leaf
-
-start = 'translation_unit'
 
 def p_primary_expression(p):
     '''
     primary_expression : ID
-	                    | CHAR_CONST
-                        | NUMBER
-	                    | OP expression CP
+	               | CHAR_CONST
+                       | NUMBER
+	               | OP expression CP
     '''
-    p[0] = Node('asdf', [p[1]])
-    
+  
 def p_postfix_expression(p):
     '''
     postfix_expression : primary_expression
-	                    | postfix_expression OSP expression CSP
-                        | postfix_expression OP CP
-                        | postfix_expression OP argument_expression_list CP
-                        | postfix_expression DOT ID
-                        | postfix_expression ARROW ID
-                        | postfix_expression INCREMENT
-                        | postfix_expression DECREMENT
+	               | postfix_expression OSP expression CSP
+                       | postfix_expression OP CP
+                       | postfix_expression OP argument_expression_list CP
+                       | postfix_expression DOT ID
+                       | postfix_expression ARROW ID
+                       | postfix_expression INCREMENT
+                       | postfix_expression DECREMENT
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_argument_expression_list(p):
-    '''
-    argument_expression_list : assignment_expression
-                            | argument_expression_list COMMA assignment_expression
-    '''
-    p[0] = Node('asdf', [p[1]])
+  '''
+  argument_expression_list : assignment_expression
+                           | argument_expression_list COMMA assignment_expression
+  '''
 
 def p_unary_expression(p):
-    '''
-    unary_expression : postfix_expression
-                        | INCREMENT unary_expression
-                        | DECREMENT unary_expression
-                        | unary_operator cast_expression
-                        | SIZEOF unary_expression
-                        | SIZEOF OP type_name CP
-    '''
-    p[0] = Node('asdf', [p[1]])
+  '''
+  unary_expression : postfix_expression
+	           | INCREMENT unary_expression
+                   | DECREMENT unary_expression
+                   | unary_operator cast_expression
+                   | SIZEOF unary_expression
+                   | SIZEOF OP type_name CP
+  '''
 
 def p_unary_operator(p):
-    '''
-    unary_operator : B_AND
-                | MULTIPLY
-                    | ADD
-                    | SUBTRACT
-                    | B_NOT
-                    | NOT
-    '''
-    p[0] = Node('asdf', [p[1]])
+  '''
+  unary_operator : B_AND
+	         | MULTIPLY
+                 | ADD
+                 | SUBTRACT
+                 | B_NOT
+                 | NOT
+  '''
 
 def p_cast_expression(p):
-    '''
-    cast_expression : unary_expression
-                    | OP type_name CP cast_expression
-    '''
-    p[0] = Node('asdf', [p[1]])
+  '''
+  cast_expression : unary_expression
+	          | OP type_name CP cast_expression
+  '''
 
 def p_multiplicative_expression(p):
-    '''
-    multiplicative_expression : cast_expression
-                            | multiplicative_expression MULTIPLY cast_expression
-                            | multiplicative_expression DIVIDE cast_expression
-                            | multiplicative_expression MOD cast_expression
-    '''
-    p[0] = Node('asdf', [p[1]])
+  '''
+  multiplicative_expression : cast_expression
+	                    | multiplicative_expression MULTIPLY cast_expression
+	                    | multiplicative_expression DIVIDE cast_expression
+	                    | multiplicative_expression MOD cast_expression
+  '''
 
 def p_additive_expression(p):
-    '''
-    additive_expression : multiplicative_expression 
-                        | additive_expression ADD multiplicative_expression
-                        | additive_expression SUBTRACT multiplicative_expression
-    '''
-    p[0] = Node('asdf', [p[1]])
+  '''
+  additive_expression : multiplicative_expression 
+                      | additive_expression ADD multiplicative_expression
+	              | additive_expression SUBTRACT multiplicative_expression
+  '''
 
 def p_shift_expression(p):
-    '''
-    shift_expression : additive_expression
-                    | shift_expression LSHIFT additive_expression
-                    | shift_expression RSHIFT additive_expression
-    '''
-    p[0] = Node('asdf', [p[1]])
-
+  '''
+  shift_expression : additive_expression
+	           | shift_expression LSHIFT additive_expression
+                   | shift_expression RSHIFT additive_expression
+  '''
+  
 def p_relational_expression(p):
-    '''
-    relational_expression : shift_expression
-                            | relational_expression LT shift_expression
-                            | relational_expression GT shift_expression
-                            | relational_expression LEQ shift_expression
-                            | relational_expression GEQ shift_expression
-    '''
-    p[0] = Node('asdf', [p[1]])
-
+  '''
+  relational_expression : shift_expression
+	                | relational_expression LT shift_expression
+                        | relational_expression GT shift_expression
+                        | relational_expression LEQ shift_expression
+                        | relational_expression GEQ shift_expression
+  '''
+  
 def p_equality_expression(p):
-    '''
-    equality_expression : relational_expression
-                        | equality_expression EQUAL relational_expression
-                        | equality_expression NEQ relational_expression
-    '''
-    p[0] = Node('asdf', [p[1]])
+  '''
+  equality_expression : relational_expression
+                      | equality_expression EQUAL relational_expression
+	              | equality_expression NEQ relational_expression
+  '''
 
 def p_and_expression(p):
-    '''
-    and_expression : equality_expression
-                    | and_expression B_AND equality_expression
-    '''
-    p[0] = Node('asdf', [p[1]])
-    
+  '''
+  and_expression : equality_expression
+	         | and_expression B_AND equality_expression
+  '''
+
 def p_exclusive_or_expression(p):
     '''
     exclusive_or_expression : and_expression
                             | exclusive_or_expression XOR and_expression
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_inclusive_or_expression(p):
     '''
     inclusive_or_expression : exclusive_or_expression
                             | inclusive_or_expression B_OR exclusive_or_expression
     '''
-    p[0] = Node('asdf', [p[1]])
-
+    
 def p_logical_and_expression(p):
     '''
     logical_and_expression : inclusive_or_expression
                            | logical_and_expression AND inclusive_or_expression
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_logical_or_expression(p):
     '''
     logical_or_expression : logical_and_expression
                           | logical_or_expression OR logical_and_expression
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_conditional_expression(p):
     '''
     conditional_expression : logical_or_expression
                            | logical_or_expression TERNARYOP expression COLON conditional_expression
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_assignment_expression(p):
     '''
     assignment_expression : conditional_expression
                           | unary_expression assignment_operator assignment_expression    
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_assignment_operator(p):
     '''
     assignment_operator : ASSIGN
-                        | MULASSIGN
-                        | DIVASSIGN
-                        | MODASSIGN
-                        | PLUSASSIGN
-                        | MINUSASSIGN
-                        | LSASSIGN
-                        | RSASSIGN
-                        | ANDASSIGN
-                        | XORASSIGN
-                        | ORASSIGN
+                        | MUL_ASSIGN
+                        | DIV_ASSIGN
+                        | MOD_ASSIGN
+                        | ADD_ASSIGN
+                        | SUB_ASSIGN
+                        | LEFT_ASSIGN
+                        | RIGHT_ASSIGN
+                        | AND_ASSIGN
+                        | XOR_ASSIGN
+                        | OR_ASSIGN
     '''
-    p[0] = Node('asdf', [p[1]])
-
 
 def p_expression(p):
     '''
     expression : assignment_expression
                | expression COMMA assignment_expression
     '''
-    p[0] = Node('asdf', [p[1]])
-
 
 def p_constant_expression(p):
     '''
     constant_expression : conditional_expression
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_declaration(p):
     '''
     declaration : declaration_specifiers SEMICOLON
-	            | declaration_specifiers init_declarator_list SEMICOLON
+	| declaration_specifiers init_declarator_list SEMICOLON
     '''
-    p[0] = Node('asdf', [p[1]])
-
+    
 def p_declaration_specifiers(p):
     '''
     declaration_specifiers : storage_class_specifier
@@ -210,21 +173,18 @@ def p_declaration_specifiers(p):
                            | type_qualifier
                            | type_qualifier declaration_specifiers
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_init_declarator_list(p):
     '''
     init_declarator_list : init_declarator
                          | init_declarator_list COMMA init_declarator
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_init_declarator(p):
     '''
     init_declarator : declarator
                     | declarator ASSIGN initializer
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_storage_class_specifier(p):
     '''
@@ -234,24 +194,22 @@ def p_storage_class_specifier(p):
                             | AUTO
                             | REGISTER
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_type_specifier(p):
     '''
-    type_specifier  : VOID
-                    | CHAR
-                    | SHORT
-                    | INT
-                    | LONG
-                    | FLOAT
-                    | DOUBLE
-                    | SIGNED
-                    | UNSIGNED
-                    | struct_or_union_specifier
-                    | enum_specifier
-                    | TYPE_NAME
+    type_specifier : VOID
+                   | CHAR
+                   | SHORT
+                   | INT
+                   | LONG
+                   | FLOAT
+                   | DOUBLE
+                   | SIGNED
+                   | UNSIGNED
+                   | struct_or_union_specifier
+                   | enum_specifier
+                   | TYPE_NAME
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_struct_or_union_specifier(p):
     '''
@@ -259,27 +217,23 @@ def p_struct_or_union_specifier(p):
                               | struct_or_union OCP struct_declaration_list CCP
                               | struct_or_union ID
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_struct_or_union(p):
     '''
     struct_or_union : STRUCT
                     | UNION
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_struct_declaration_list(p):
     '''
     struct_declaration_list : struct_declaration
                             | struct_declaration_list struct_declaration
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_struct_declaration(p):
     '''
     struct_declaration : specifier_qualifier_list struct_declarator_list SEMICOLON
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_specifier_qualifier_list(p):
     '''
@@ -288,14 +242,12 @@ def p_specifier_qualifier_list(p):
                              | type_qualifier specifier_qualifier_list
                              | type_qualifier
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_struct_declarator_list(p):
     '''
     struct_declarator_list : struct_declarator
                            | struct_declarator_list COMMA struct_declarator
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_struct_declarator(p):
     '''
@@ -303,7 +255,6 @@ def p_struct_declarator(p):
                       | COLON constant_expression
                       | declarator COLON constant_expression
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_enum_specifier(p):
     '''
@@ -311,35 +262,30 @@ def p_enum_specifier(p):
                    | ENUM ID OCP enumerator_list CCP
                    | ENUM ID
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_enumerator_list(p):
     '''
     enumerator_list : enumerator
                     | enumerator_list COMMA enumerator
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_enumerator(p):
     '''
     enumerator : ID
                | ID ASSIGN constant_expression
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_type_qualifier(p):
     '''
     type_qualifier : CONST
                    | VOLATILE
     '''
-    p[0] = Node('asdf', [p[1]])
-
+	
 def p_declarator(p):
     '''
     declarator : pointer direct_declarator
                | direct_declarator
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_direct_declarator(p):
     '''
@@ -351,7 +297,6 @@ def p_direct_declarator(p):
                       | direct_declarator OP identifier_list CP
                       | direct_declarator OP CP
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_pointer(p):
     '''
@@ -360,28 +305,24 @@ def p_pointer(p):
             | MULTIPLY pointer
             | MULTIPLY type_qualifier_list pointer
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_type_qualifier_list(p):
     '''
     type_qualifier_list : type_qualifier
                         | type_qualifier_list type_qualifier
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_parameter_type_list(p):
     '''
     parameter_type_list : parameter_list
                         | parameter_list COMMA ELLIPSIS
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_parameter_list(p):
     '''
     parameter_list : parameter_declaration
                    | parameter_list COMMA parameter_declaration
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_parameter_declaration(p):
     '''
@@ -389,21 +330,18 @@ def p_parameter_declaration(p):
                           | declaration_specifiers abstract_declarator
                           | declaration_specifiers
     '''
-    p[0] = Node('asdf', [p[1]])
-
+	
 def p_identifier_list(p):
     '''
     identifier_list : ID
                     | identifier_list COMMA ID
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_type_name(p):
     '''
     type_name : specifier_qualifier_list
               | specifier_qualifier_list abstract_declarator
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_abstract_declarator(p):
     '''
@@ -411,7 +349,6 @@ def p_abstract_declarator(p):
                         | direct_abstract_declarator
                         | pointer direct_abstract_declarator
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_direct_abstract_declarator(p):
     '''
@@ -425,7 +362,6 @@ def p_direct_abstract_declarator(p):
                                | direct_abstract_declarator OP CP
                                | direct_abstract_declarator OP parameter_type_list CP
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_initializer(p):
     '''
@@ -433,14 +369,12 @@ def p_initializer(p):
                 | OCP initializer_list CCP
                 | OCP initializer_list COMMA CSP
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_initializer_list(p):
     '''
     initializer_list : initializer
                      | initializer_list COMMA initializer
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_statement(p):
     '''
@@ -451,15 +385,13 @@ def p_statement(p):
               | iteration_statement
               | jump_statement
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_labeled_statement(p):
     '''
     labeled_statement : ID COLON statement
                       | CASE constant_expression COLON statement
-                      | DEFAULT COLON statement
+                      | DEFAULT COLON' statement
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_compound_statement(p):
     '''
@@ -468,28 +400,24 @@ def p_compound_statement(p):
                        | OCP declaration_list CCP
                        | OCP declaration_list statement_list CCP
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_declaration_list(p):
     '''
     declaration_list : declaration
                      | declaration_list declaration
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_statement_list(p):
     '''
     statement_list : statement
                    | statement_list statement
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_expression_statement(p):
     '''
     expression_statement : SEMICOLON
                          | expression SEMICOLON
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_selection_statement(p):
     '''
@@ -497,8 +425,6 @@ def p_selection_statement(p):
                         | IF OP expression CP statement ELSE statement
                         | SWITCH OP expression CP statement
     '''
-    if(len(p) == 6): p[0] = Node('if-then', [p[3], p[5]], None)
-    elif(len(p) == 8): p[0] = Node('if-then-else', [p[3], p[5], p[7]], None)
 	
 def p_iteration_statement(p):
     '''
@@ -507,7 +433,6 @@ def p_iteration_statement(p):
                         | FOR OP expression_statement expression_statement CP statement
                         | FOR OP expression_statement expression_statement expression CP statement
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_jump_statement(p):
     '''
@@ -517,22 +442,19 @@ def p_jump_statement(p):
                    | RETURN SEMICOLON
                    | RETURN expression SEMICOLON
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_translation_unit(p):
     '''
     translation_unit : external_declaration
                      | translation_unit external_declaration
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_external_declaration(p):
     '''
     external_declaration : function_definition
                          | declaration
     '''
-    p[0] = Node('asdf', [p[1]])
-
+	
 def p_function_definition(p):
     '''
     function_definition : declaration_specifiers declarator declaration_list compound_statement
@@ -540,11 +462,7 @@ def p_function_definition(p):
                         | declarator declaration_list compound_statement
                         | declarator compound_statement
     '''
-    p[0] = Node('asdf', [p[1]])
 
 def p_error(p):
-    print("error for ", p)
-    print("Syntax Error found at ", p.lineno)
-    
-
-parser = yacc.yacc(debug=1)
+    print "Syntax Error"
+    sys.exit()
