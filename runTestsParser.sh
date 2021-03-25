46abb1ecbb2c08de4c27aae1690bbddcc106b892
mnithya1 <62048822+mnithya1@users.noreply.github.com>
2021-03-25 15:24:12 +0530

    Update runTestsParser.sh
    dot command

--- runTestsParser.sh
+++ runTestsParser.sh
@@ -6,11 +6,16 @@ test () {
 }
 
 # tests
-test 1 
+test 1
+dot -Tps output_raw.dot -o tree_result1.ps
 test 2
+dot -Tps output_raw.dot -o tree_result2.ps
 test 3
+dot -Tps output_raw.dot -o tree_result3.ps
 test 4
+dot -Tps output_raw.dot -o tree_result4.ps
 test 5
+dot -Tps output_raw.dot -o tree_result5.ps
 
 # cleanup pycache and out files
 [ -d ./src/__pycache__ ] && rm -rf ./src/__pycache__
