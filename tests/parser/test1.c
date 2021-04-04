0d90acf2b7e8769b1af152a04a1b4c17e1d783f9
mnithya1 <62048822+mnithya1@users.noreply.github.com>
2021-04-04 16:16:17 +0530

    Update test1.c

--- tests/parser/test1.c
+++ tests/parser/test1.c
@@ -1,10 +1,12 @@
-int main(){
-    int a = 5 ;
-    int b = a*10 ;
-    int d = b-7 ;
-    int e = a+b+c+d ;
-    float f = a+b/10 ;
-    double k = 10.25748*10.490904503 ;
-    long l = 100000000*1000000000 ;
-    return 0;
+
+struct area{
+	int l;
+	int b;
+};
+
+int main()
+{
+	int x;
+	char y = x;
+	return 0;
 }
