f62f87ac55e19f8e59d2178038d1da852da5a1f7
shashankk-agarwal <shashank4120@gmail.com>
2021-04-18 03:06:11 +0530

    Error checking

--- tests/parser/test2.c
+++ tests/parser/test2.c
@@ -1,7 +1,31 @@
+int mult(int a, int b){
+	return a*b ;
+}
+
 int main(int argc){
     int a = 1, b = 2;
-    if(a < b){
-        a++;
+    int c ;
+    int i=0 ; 
+    int arr[9] ;
+    int *x ;
+    x = arr ;
+    while(1){
+    	a = b + 4.4 ;
+    }
+    for(i=0 ; i<10 ; i++){
+    	int x = 5 ;
+    	b = a - 4.4 ;
+    	x = x*b ;
+    }
+    if(1+2){
+    	c = mult(a, b) ;
+    }
+    else{
+    	c = -1 ;
+    }
+    
+    for(i=0 ; i<9 ; i++){
+    	arr[i] = 7 ;
     }
     return 0;
 }
