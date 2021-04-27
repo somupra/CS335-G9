0110f068b41254d8d6366c381ef534998f5bbacf
shashankk-agarwal <shashank4120@gmail.com>
2021-04-28 01:57:41 +0530

    Test case updated

--- tests/parser/test1.c
+++ tests/parser/test1.c
@@ -1,5 +1,28 @@
 int main()
 {
 	int a ;
-	a = 5 ;
+	int b ;
+	int i ;
+	a = 7 ;
+	b = 5 ;
+	if(a>b){
+		int x ;
+		x = 7.2 ;
+	}
+	else{
+		int y ;
+		y = 99 ;
+	}
+	b = 11 ;
+	
+	while(a>b){
+		a = a+1 ;
+		b = b-1 ;
+	}
+	a = b ;
+	
+	for(a=0 ; a<5 ; a=2*a ){
+		b = b-a ;
+	}
+	b = a-1 ;
 }
