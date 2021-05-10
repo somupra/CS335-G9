52c570a0a4e32915f6d17d4ebc03ee20f97ea4bd
mnithya1 <62048822+mnithya1@users.noreply.github.com>
2021-05-10 21:09:06 +0530

    Update code.py

--- src/code.py
+++ src/code.py
@@ -1,15 +1,22 @@
 import symbol_table
 from parser.grammar import instr
 
-
+current_function = 'main'
 code = []
 regs = ['RBX','RCX','RDX','RSI','RDI']
 
 def param(x):
-	code.append('pushq '+str(x))
+	# x can be int, float, char or tmp@
+	if '@' in x:
+		# temporary var : To be completed
+		code.append('')
+	else:
+		# Integer or char : push 0x10 push 'a'
+		code.append('push '+x)
 
-def call_begin(ID,name):
+def call_begin(x):
 	#Fulfills both caller and callee partially.
+	name = x[:x.find(',')]
 	# CALLER PART
 	code.append('push rax')
 	code.append('push rbx')
@@ -17,30 +24,8 @@ def call_begin(ID,name):
 	code.append('push rdx')
 	code.append('push rsi')
 	code.append('push rdi')
-	code.append('call '+str(ID))# is this the right instr? call func? func is the label here before the function code, similar to goto labels.
-	# CALLEE PART
-	code.append('push rbp')
-	code.append('mov  rsp, rbp')
-	# Getting the symtab entry details of function using the function name.
-	func_details = symbol_table.check_in_fs(name)
-	symtab_id = func_details[0]
-	no_of_params = func_details[1]
-	variables = symbol_table.allsymboltables[symtab_id].variables
-	no_of_vars = len(variables)
-	i=0
-	space_for_variables = 0
-	for x in variables:
-		if i== (no_of_vars - no_of_params):
-			space_for_variables = variables[x]['offset']
-			break
-		i+=1
-	# Allocate space for local variables
-	code.append('subb rsp, '+str(space_for_variables))
-
-
-def call_end(name):
-	# pop the regs
-	# Pop the parameters and put the return value from eax into ...
+	code.append('call '+name)# is this the right instr? call func? func is the label here before the function code, similar to goto labels.
+	# CALL END, CALLER PART :
 	code.append('pop rdi')
 	code.append('pop rsi')
 	code.append('pop rdx')
@@ -51,12 +36,15 @@ def call_end(name):
 	func_details = symbol_table.check_in_fs(name)
 	# func_details - 0 is table id and 1 is no of params
 	space_for_params = func_details[1]*8
-	code.append('addb rsp, '+str(space_for_params))
+	code.append('add rsp, byte '+str(space_for_params))
+
 
 def returnn(ret_val):
 	# Assuming 8Bytes and that ret_val is the memory address that has the return value
-	code.append('movq '+str(ret_val)+', %rax')
-	code.append('mov %rbp, %rsp') # move ebp into esp  
+	code.append('mov rax, '+str(ret_val))
+	if current_function == 'main':
+		return
+	code.append('mov rsp, rbp') # move ebp into esp  
 	code.append('pop rbp')
 	code.append('ret')
 
@@ -77,12 +65,51 @@ def print_call():
 	code.append('pop rax')
 
 
+def func_begin(name):
+	code.append(name)# name consists : too. so label done
+	# CALLEE PART
+	name = name[:-1]
+	global current_function
+	current_function = name
+	if name == 'main':
+		return
+	code.append('push rbp')
+	code.append('mov  rbp, rsp')
+	# Getting the symtab entry details of function using the function name.
+	func_details = symbol_table.check_in_fs(name)
+	symtab_id = func_details[0]
+	no_of_params = func_details[1]
+	variables = symbol_table.allsymboltables[symtab_id].variables
+	no_of_vars = len(variables)
+	i=0
+	space_for_variables = 0
+	for x in variables:
+		if i== (no_of_vars - no_of_params):
+			space_for_variables = variables[x]['offset']
+			break
+		i+=1
+	# Allocate space for local variables
+	code.append('sub rsp, byte '+str(space_for_variables))
 
-def main():
-	return
+
+def assembly():
+	code.append('global main')
+	code.append('section .text')
 	for x in instr:
+		print(x)
 		if x=='printcall':
 			print_call()
+		elif x.find(':')!=-1:
+			# A function declaration
+			func_begin(x)
+		elif x[:5]=='param':
+			# param being declared
+			param(x[6:])
+		elif x[:4]=='call':
+			call_begin(x[5:])
+		elif x[:6]=='return':
+			returnn(x[7:])
+
 	f = open("./src/assembly.asm", "w")
 	for x in code:
 		f.write(x)
